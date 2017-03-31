d0181a4c9a8376bd89cbc31032457fb2f39a70b9
chiguokun <chiguokun@outlook.com>
2017-03-31 19:56:32 +0800

    The first commit

---
+++ OfficeAutomation/src/dlmu/oa/test/SpringTest.java
@@ -0,0 +1,25 @@
+package dlmu.oa.test;
+
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+
+	private ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+	// 测试SessionFactory
+	@Test
+	public void testSessionFactory() throws Exception {
+		SessionFactory sessionFactory = (SessionFactory) ac.getBean("sessionFactory");
+		System.out.println(sessionFactory);
+	}
+
+	// 测试事务
+	
+	public void testTransaction() throws Exception {
+		TestService testService  = (TestService) ac.getBean("testService");
+		testService.saveTwoUsers();
+	}
+}
