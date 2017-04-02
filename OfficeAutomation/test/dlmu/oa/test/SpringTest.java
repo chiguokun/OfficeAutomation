24ca77b446004f7990c5fde4eae5f2fcca7771fd
chiguokun <chiguokun@outlook.com>
2017-04-02 16:44:04 +0800

    add log jar

--- OfficeAutomation/test/dlmu/oa/test/SpringTest.java
+++ OfficeAutomation/test/dlmu/oa/test/SpringTest.java
@@ -1,5 +1,7 @@
 package dlmu.oa.test;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -8,16 +10,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringTest {
 
 	private ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-
+	
+	private Log log = LogFactory.getLog(getClass());
+	
+	@Test
+	public void testLog() throws Exception{
+		log.debug("debug信息");
+		log.info("info信息");
+		log.warn("warn信息");
+		log.error("error信息");
+	}
+	
 	// 测试SessionFactory
 	@Test
 	public void testSessionFactory() throws Exception {
 		SessionFactory sessionFactory = (SessionFactory) ac.getBean("sessionFactory");
 		System.out.println(sessionFactory);
 	}
-
-	// 测试事务
 	
+	// 测试事务
+	@Test
 	public void testTransaction() throws Exception {
 		TestService testService  = (TestService) ac.getBean("testService");
 		testService.saveTwoUsers();
